1c8ef0d41a9cc62d775835ba995d6600ada55a8f
Alexis Prevost-Maynen <alexis@callbruno.com>
2022-10-20 12:11:40 +0200

    Update - Add hover & script for slider

--- Extern/script.js
+++ Extern/script.js
@@ -1,1 +1,38 @@
-console.log("github script");
+window.addEventListener('load', function () {
+
+    if ( document.querySelector('.section-hero-tl') ) {
+        if ( document.querySelector('.section-quotes-b') ) {
+            
+            document.querySelector('.section-quotes-b .section-quotes-b-containerimg .section-quotes-b-containerimg-imgs:first-child').classList.add('active');
+            document.querySelector('.section-quotes-b .section-quotes-b-containertext .section-quotes-b-containerquotes .section-quotes-b-containerquotes-quote:first-child').classList.add('active');
+
+            let indexSectionQuote = 1;
+            let indexMaxSectionQuote = document.querySelectorAll('.section-quotes-b .section-quotes-b-containerimg .section-quotes-b-containerimg-imgs').length;
+
+            document.querySelector('.section-quotes-b .section-quotes-b-containerarrows img:first-child').addEventListener('click', function(){
+
+                document.querySelector('.section-quotes-b .section-quotes-b-containerimg .section-quotes-b-containerimg-imgs:nth-child('+indexSectionQuote+')').classList.remove('active');
+                document.querySelector('.section-quotes-b .section-quotes-b-containertext .section-quotes-b-containerquotes .section-quotes-b-containerquotes-quote:nth-child('+indexSectionQuote+')').classList.remove('active');
+
+                indexSectionQuote--;
+                if(indexSectionQuote < 1) indexSectionQuote = indexMaxSectionQuote;
+
+                document.querySelector('.section-quotes-b .section-quotes-b-containerimg .section-quotes-b-containerimg-imgs:nth-child('+indexSectionQuote+')').classList.add('active');
+                document.querySelector('.section-quotes-b .section-quotes-b-containertext .section-quotes-b-containerquotes .section-quotes-b-containerquotes-quote:nth-child('+indexSectionQuote+')').classList.add('active');
+            });
+
+            document.querySelector('.section-quotes-b .section-quotes-b-containerarrows img:last-child').addEventListener('click', function(){
+                
+                document.querySelector('.section-quotes-b .section-quotes-b-containerimg .section-quotes-b-containerimg-imgs:nth-child('+indexSectionQuote+')').classList.remove('active');
+                document.querySelector('.section-quotes-b .section-quotes-b-containertext .section-quotes-b-containerquotes .section-quotes-b-containerquotes-quote:nth-child('+indexSectionQuote+')').classList.remove('active');
+
+                indexSectionQuote++;
+                if(indexSectionQuote > indexMaxSectionQuote) indexSectionQuote = 1;
+
+                document.querySelector('.section-quotes-b .section-quotes-b-containerimg .section-quotes-b-containerimg-imgs:nth-child('+indexSectionQuote+')').classList.add('active');
+                document.querySelector('.section-quotes-b .section-quotes-b-containertext .section-quotes-b-containerquotes .section-quotes-b-containerquotes-quote:nth-child('+indexSectionQuote+')').classList.add('active');
+            });
+        }    
+    }
+
+})
